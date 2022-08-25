5c564a35c81ce5dd36033daad43254314c5177ea
Klee Thomas <klee.ut@gmail.com>
2022-08-25 20:48:57 +1000

    :pencil: cleaning up typos

--- src/domain/questionFactory.ts
+++ src/domain/questionFactory.ts
@@ -5,18 +5,17 @@ export function questionFactory(): Question[] {
       {
         scores: [1, 2, 3],
         priority: 1,
-        text: `There is no way to be truely agile if you are not able to iterate and change quickly. 
-        Anything you can do to decrease the time it takes to deiver to production will increase your agility. Allowing you to respond to chnage rather than follwing a plan.`,
+        text: "There is no way to be truly agile if you are not able to iterate and change quickly. Anything you can do to decrease the time it takes to deliver to production will increase your agility. Allowing you to respond to change rather than following a plan.",
       },
       {
         scores: [4],
         priority: 20,
-        text: `Looks like you're doing great. If anything deploying to production a little more often coud make you even better`,
+        text: "Looks like you're doing great. If anything deploying to production a little more often could make you even better.",
       },
       {
         scores: [5],
         priority: 99,
-        text: `Looks like you're doing great. But you already knew that.`,
+        text: "Looks like you're doing great. But you already knew that",
       },
     ],
     options: [
@@ -82,8 +81,7 @@ export function questionFactory(): Question[] {
       {
         scores: [1, 2, 3],
         priority: 3,
-        text: `Collaboration is a key factor in ensuring that a team is functional. 
-        Without good lines of communication a team will ship the wrong thing or spend time reworking as stake holders clarify requirements after the work is complete.`,
+        text: "Collaboration is a key factor in ensuring that a team is functional. Without good lines of communication a team will ship the wrong thing or spend time reworking as stake holders clarify requirements after the work is complete.",
       },
     ],
     options: [
@@ -97,15 +95,15 @@ export function questionFactory(): Question[] {
       },
       {
         score: 3,
-        text: "Good most of the time we have all the people we need in the room.",
+        text: "Good most of the time we have all the people we need in the room",
       },
       {
         score: 2,
-        text: "Poor, we often feel like we're working with half the data.",
+        text: "Poor, we often feel like we're working with half the data",
       },
       {
         score: 1,
-        text: "What communicaton?",
+        text: "What communication?",
       },
     ],
   });
@@ -116,30 +114,29 @@ export function questionFactory(): Question[] {
       {
         scores: [1, 2, 3],
         priority: 4,
-        text: `At the heart of truely agile teams is self organisation. 
-        The team should own the process and feel empowered to adjust those processes as they see fit`,
+        text: "At the heart of truly agile teams is self organization. The team should own the process and feel empowered to adjust those processes as they see fit",
       },
     ],
     options: [
       {
         score: 5,
-        text: "Every retrospective.",
+        text: "Every retrospective",
       },
       {
         score: 4,
-        text: "Often.",
+        text: "Often",
       },
       {
         score: 3,
-        text: "When the suggestions are good enough to take to management.",
+        text: "When the suggestions are good enough to take to management",
       },
       {
         score: 2,
-        text: "Rarely if ever. The processes are set out by managment.",
+        text: "Rarely if ever. The processes are set out by management",
       },
       {
         score: 1,
-        text: "We dont have retrospectives.",
+        text: "We don't have retrospectives",
       },
     ],
   });
@@ -150,14 +147,13 @@ export function questionFactory(): Question[] {
       {
         scores: [1, 2, 3],
         priority: 5,
-        text: `If you can't see what your users are doing, where they're having problems and what features they're using how can you be confident that you are truely delivierng value to your customers?
-        `,
+        text: "Seems like it's time to get some production monitoring in place. If you can't see what your users are doing, where they're having problems and what features they're using how can you be confident that you are truly delivering value to your customers?",
       },
     ],
     options: [
       {
         score: 5,
-        text: "Very confident, we know about probelms before our users do.",
+        text: "Very confident, we know about problem before our users do.",
       },
       {
         score: 4,
@@ -165,7 +161,7 @@ export function questionFactory(): Question[] {
       },
       {
         score: 3,
-        text: "Somewaht, we have some logs they can usually help us solve issues.",
+        text: "Somewhat, we have some logs they can usually help us solve issues.",
       },
       {
         score: 2,
@@ -180,58 +176,36 @@ export function questionFactory(): Question[] {
 
   const vision = (): Question => ({
     text: "How well would you say the members of the team can describe the product vision?",
-    advice: [],
+    advice: [
+      {
+        scores: [1, 2, 3],
+        priority: 4,
+        text: "It would be a good idea to better communicate your product vision to the team. Getting the whole team on the same page about product vision means that everyone is empowered to make decisions that move the product forwards",
+      },
+    ],
     options: [
       {
         score: 5,
-        text: "We all share a vision and can communicate it effectively.",
+        text: "We all share a vision and can communicate it effectively",
       },
       {
         score: 4,
-        text: "The senior members of the team share a vision and can communicate it.",
+        text: "The senior members of the team share a vision and can communicate it",
       },
       {
         score: 3,
-        text: "The product manager has a vision and is available to communicate it.",
+        text: "The product manager has a vision and is available to communicate it",
       },
       {
         score: 2,
-        text: "There is a vision but it is not well defined.",
+        text: "There is a vision but it is not well defined",
       },
       {
         score: 1,
-        text: "I'm not sure that there is a vision.",
+        text: "I'm not sure that there is a vision",
       },
     ],
   });
-
-  // Work out how to word this
-  // const visionDelivery = (): Question => ({
-  //   text: "How empowered is the team to deliver the product vision?",
-  //   advice: [],
-  //   options: [
-  //     {
-  //       score: 5,
-  //       text: "The team is free to make decisions that help us get to where we need to go.",
-  //     },
-  //     {
-  //       score: 4,
-  //       text: "The senior members of the team share a vision and can communicate it.",
-  //     },
-  //     {
-  //       score: 3,
-  //       text: "The product manager has a vision and is available to communicate it.",
-  //     },
-  //     {
-  //       score: 2,
-  //       text: "There is a vision but it is not well defined.",
-  //     },
-  //     {
-  //       score: 1,
-  //       text: "I'm not sure that there is a vision.",
-  //     },
-  //   ],
-  // });
 
   return [
     deployFrequency(),
@@ -240,6 +214,5 @@ export function questionFactory(): Question[] {
     vision(),
     retrospectives(),
     monitoring(),
-    // visionDelivery(),
   ];
 }
